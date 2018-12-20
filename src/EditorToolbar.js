09935765189473bf26c80ab570ee34fddfd5ac0d
Rob Richard <rob@1stdibs.com>
2018-12-20 13:03:06 -0500

    button padding

--- src/EditorToolbar.js
+++ src/EditorToolbar.js
@@ -1,18 +1,25 @@
 import React, { useContext } from 'react';
 import { RecessContext } from './RecessContext';
 import Button from './Button';
+import styles from './styles/EditorToolbar.module.css';
 
 export default function EditorToolbar() {
     const { useCamelCase, setCamelCase, formatRequest, insertMock } = useContext(RecessContext);
     return (
         <React.Fragment>
-            <Button onClick={formatRequest}>Format</Button>{' '}
-            <Button onClick={insertMock}>Insert Mock</Button>{' '}
-            {useCamelCase ? (
-                <Button onClick={() => setCamelCase(false)}>Use snake_case</Button>
-            ) : (
-                <Button onClick={() => setCamelCase(true)}>Use camelCase</Button>
-            )}
+            <div className={styles.buttonWrapper}>
+                <Button onClick={formatRequest}>Format</Button>
+            </div>
+            <div className={styles.buttonWrapper}>
+                <Button onClick={insertMock}>Insert Mock</Button>{' '}
+            </div>
+            <div className={styles.buttonWrapper}>
+                {useCamelCase ? (
+                    <Button onClick={() => setCamelCase(false)}>Use snake_case</Button>
+                ) : (
+                    <Button onClick={() => setCamelCase(true)}>Use camelCase</Button>
+                )}
+            </div>
         </React.Fragment>
     );
 }
