d12aa36dd6824d8c9e1e04bce078336d318fbd51
Jack <30052269+jackloughran@users.noreply.github.com>
2019-10-03 10:47:17 -0400

    add deployment script (#2)

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# make sure docker is running
+
+yarn build
+
+# copied from ecr instructions
+$(aws ecr get-login --no-include-email --region us-east-1)
+docker build -t 1stdibs-recess .
+docker tag 1stdibs-recess:latest 268215509542.dkr.ecr.us-east-1.amazonaws.com/1stdibs-recess:latest
+docker push 268215509542.dkr.ecr.us-east-1.amazonaws.com/1stdibs-recess:latest
+
+# ecs deploy one-liner from Joey
+aws --region us-east-1 ecs update-service --service 1stdibs-recess-service-lb2 --cluster recess-cluster --force-new-deployment
