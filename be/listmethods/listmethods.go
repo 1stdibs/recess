63ac17649ca6ff6f21383cb9592195cd515dda97
Jack Loughran <30052269+jackloughran@users.noreply.github.com>
2020-02-07 14:43:28 -0500

    adjust autocomplete to use separate type list

--- be/listmethods/listmethods.go
+++ be/listmethods/listmethods.go
@@ -3,11 +3,11 @@ package listmethods
 import (
 	"fmt"
 
-	"github.com/jhump/protoreflect/grpcreflect"
 	"github.com/1stdibs/recess/be/recess"
+	"github.com/jhump/protoreflect/grpcreflect"
 )
 
-func ListMethods(client *grpcreflect.Client, service string) ([]*recess.Method, error) {
+func ListMethods(client *grpcreflect.Client, service string) ([]recess.Field, error) {
 	fd, err := client.FileContainingSymbol(service)
 	if err != nil {
 		return nil, fmt.Errorf("couldn't find service %s: %v", service, err)
@@ -21,18 +21,21 @@ func ListMethods(client *grpcreflect.Client, service string) ([]*recess.Method, 
 
 	methods := serviceDescriptor.GetMethods()
 
-	result := make([]*recess.Method, len(methods))
+	result := make([]recess.Field, len(methods))
 	for i, m := range methods {
-		result[i] = &recess.Method{
+		result[i] = recess.Field{
 			Name: m.GetName(),
-			Type: getTypeName(m.IsClientStreaming(), m.IsServerStreaming()),
+			Type: recess.Type{
+				Name: m.GetFullyQualifiedName(),
+				Kind: getKindName(m.IsClientStreaming(), m.IsServerStreaming()),
+			},
 		}
 	}
 
 	return result, nil
 }
 
-func getTypeName(isClientStreaming, isServerStreaming bool) string {
+func getKindName(isClientStreaming, isServerStreaming bool) string {
 	if isClientStreaming && isServerStreaming {
 		return "bidi_streaming"
 	} else if isClientStreaming && !isServerStreaming {
--- be/recess/recess.go
+++ be/recess/recess.go
@@ -1,23 +1,32 @@
 package recess
 
-type Method struct {
-	Name   string   `json:"name"`
-	Type   string   `json:"type"`
-	Fields []*Field `json:"fields,omitempty"`
-}
-
-type Service struct {
-	Name    string    `json:"serviceName"`
-	Methods []*Method `json:"methods"`
+type Type struct {
+	Name       string   `json:"name"`
+	Fields     []Field  `json:"fields,omitempty"`
+	Kind       string   `json:"kind,omitempty"`
+	EnumValues []string `json:"enumValues,omitempty"`
 }
 
 type Field struct {
-	Name       string   `json:"name"`
-	IsRepeated bool     `json:"isRepeated"`
-	IsRequired bool     `json:"isRequired"`
-	Type       string   `json:"type"`
-	EnumValues []string `json:"enumValues,omitempty"`
-	Children   []*Field `json:"children,omitempty"`
+	Name       string `json:"name"`
+	IsRepeated bool   `json:"isRepeated"`
+	IsRequired bool   `json:"isRequired"`
+	Type       Type   `json:"type"` // only the name should be inflated to avoid circular dependencies
+}
+
+type Method struct {
+	Name      string `json:"name"`
+	InputType Type   `json:"inputType"`
+}
+
+type Service struct {
+	Name    string   `json:"name"`
+	Methods []Method `json:"methods"`
+}
+
+type AutocompleteData struct {
+	Services []Service `json:"services"`
+	Types    []Type    `json:"types"`
 }
 
 // IsCamelCase is used by all requests/responses that convert proto <-> json
