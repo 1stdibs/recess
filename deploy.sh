f0bd1ce8a30c617b34a6e8efbfc0fef0fcec2aac
Rob Richard <rob@1stdibs.com>
2020-11-05 17:59:15 -0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,7 @@ yarn build
 docker --version
 docker pull golang
 # copied from ecr instructions
+aws --version
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 268215509542.dkr.ecr.us-east-1.amazonaws.com
 docker build -t 1stdibs-recess .
 docker tag 1stdibs-recess:latest 268215509542.dkr.ecr.us-east-1.amazonaws.com/1stdibs-recess:latest
