b9ed2111a8494afe4b647ac9d9fececb31f2a26f
Joey Carmello <joey.carmello@1stdibs.com>
2020-12-17 15:57:03 -0500

    ready to go

--- deploy.sh
+++ deploy.sh
@@ -4,22 +4,21 @@ set -e
 shopt -s expand_aliases
 if [ "$JENKINS_BUILD" = "true" ]; then
 alias aws="${AWSCLIV2}"
+type aws
 else
 echo "This isn't a Jenkins build, so we will use the local aws-cli version."
+type aws
 fi
 
-
-type aws
-aws s3 ls
-#yarn build
-#docker --version
-#docker pull golang
+yarn build
+docker --version
+docker pull golang
 ## copied from ecr instructions
-#aws --version
-#aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 268215509542.dkr.ecr.us-east-1.amazonaws.com
-#docker build -t 1stdibs-recess .
-#docker tag 1stdibs-recess:latest 268215509542.dkr.ecr.us-east-1.amazonaws.com/1stdibs-recess:latest
-#docker push 268215509542.dkr.ecr.us-east-1.amazonaws.com/1stdibs-recess:latest
+aws --version
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 268215509542.dkr.ecr.us-east-1.amazonaws.com
+docker build -t 1stdibs-recess .
+docker tag 1stdibs-recess:latest 268215509542.dkr.ecr.us-east-1.amazonaws.com/1stdibs-recess:latest
+docker push 268215509542.dkr.ecr.us-east-1.amazonaws.com/1stdibs-recess:latest
 #
 ## ecs deploy one-liner from Joey
-#aws --region us-east-1 ecs update-service --service recess-service --cluster recess-cluster --force-new-deployment
+aws --region us-east-1 ecs update-service --service recess-service --cluster recess-cluster --force-new-deployment
