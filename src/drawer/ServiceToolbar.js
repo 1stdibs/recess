4d597bb0daff4cb60303ba3b9818650a39849a3b
Rob Richard <rob@1stdibs.com>
2021-09-15 10:07:06 -0400

    fix debounce

--- src/drawer/ServiceToolbar.js
+++ src/drawer/ServiceToolbar.js
@@ -3,17 +3,36 @@ import React, { useState, useEffect } from 'react';
 import styles from './styles/ServiceToolbar.module.css';
 import { ReactComponent as SearchIcon } from '../icons/filter.svg';
 import { ReactComponent as ClearIcon } from '../icons/clear filter.svg';
-import debounce from 'lodash.debounce';
 
-export default function Service({ service, onChange, placeholder = 'Search' }) {
-    const [searchText, setSearchText] = useState('');
-
+// from https://usehooks.com/useDebounce/
+function useDebounce(value, delay) {
+    // State and setters for debounced value
+    const [debouncedValue, setDebouncedValue] = useState(value);
     useEffect(
-        debounce(() => {
-            onChange(searchText);
-        }, 300),
-        [searchText, onChange]
+        () => {
+            // Update debounced value after delay
+            const handler = setTimeout(() => {
+                setDebouncedValue(value);
+            }, delay);
+            // Cancel the timeout if value changes (also on delay change or unmount)
+            // This is how we prevent debounced value from updating if value is changed ...
+            // .. within the delay period. Timeout gets cleared and restarted.
+            return () => {
+                clearTimeout(handler);
+            };
+        },
+        [value, delay] // Only re-call effect if value or delay changes
     );
+    return debouncedValue;
+}
+
+export default function ServiceToolbar({ service, onChange, placeholder = 'Search' }) {
+    const [searchText, setSearchText] = useState('');
+    const debouncedSearchTerm = useDebounce(searchText, 300);
+
+    useEffect(() => {
+        onChange(debouncedSearchTerm);
+    }, [debouncedSearchTerm, onChange]);
 
     const icon = searchText ? (
         <ClearIcon className={`${styles.icon} ${styles.clear}`} onClick={() => setSearchText('')} />
@@ -27,7 +46,7 @@ export default function Service({ service, onChange, placeholder = 'Search' }) {
                 className={styles.wrapper}
                 placeholder={placeholder}
                 value={searchText}
-                onChange={e => setSearchText(e.target.value)}
+                onChange={(e) => setSearchText(e.target.value)}
             />
             {icon}
         </>
